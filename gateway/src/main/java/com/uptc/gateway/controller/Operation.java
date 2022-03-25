3c55b07cae9a41ad2492d37c5f7e6f4efeaafd4e
MaicolM11 <mmoarias4@gmail.com>
2022-03-24 21:59:38 -0500

    ahora si

---
+++ gateway/src/main/java/com/uptc/gateway/controller/Operation.java
@@ -0,0 +1,26 @@
+package com.uptc.gateway.controller;
+
+public class Operation {
+    
+    double numberA, numberB;
+    String operation;
+    public double getNumberA() {
+        return numberA;
+    }
+    public void setNumberA(double numberA) {
+        this.numberA = numberA;
+    }
+    public double getNumberB() {
+        return numberB;
+    }
+    public void setNumberB(double numberB) {
+        this.numberB = numberB;
+    }
+    public String getOperation() {
+        return operation;
+    }
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
+    
+}
